fe11d00256180bd8860fcc20c251bc33016d646e
Sommaly <vanh.sommaly@gmail.com>
2023-09-20 11:52:04 +0200

    classe eleve

---
+++ McStudent/Classe/Eleve.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McStudent
+{
+    internal class Eleve
+    {
+        string pseudo;
+        string prenom;
+        string motdepasse;
+
+        public Eleve(string pseudo, string prenom, string motdepasse)
+        {
+            this.pseudo = pseudo;
+            this.prenom = prenom;
+            this.motdepasse = motdepasse;
+        }
+
+        public string getPseudo()
+        {
+            return this.pseudo;
+        }
+        public string getPrenom()
+        {
+            return this.prenom;
+        }
+        public void setMdp(String nvMotdepasse)
+        {
+            this.motdepasse = nvMotdepasse;
+        }
+        public void setPseudo(String nvPseudo)
+        {
+            this.pseudo = nvPseudo;
+        }
+    }
+}
