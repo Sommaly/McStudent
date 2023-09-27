d684223b271086a7d867ce2d07e3a7291028aa5a
Sommaly <vanh.sommaly@gmail.com>
2023-09-27 09:16:23 +0200

    modification eleve tache

---
+++ McStudent/Classe/EleveTache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McStudent.Classe
+{
+    internal class EleveTache
+    {
+        public bool etat { get; set; }
+        public int ptsRetires { get; set; }
+        public EleveTache(int ptsRetires)
+        {
+            etat = false;
+            this.ptsRetires = ptsRetires;
+        }
+    }
+}
