b7e7763b5c3678c7d71352e92f1f416e15c098d7
Ralsywy <bichonkevin4@gmail.com>
2023-10-04 10:58:25 +0200

    suppr btn

--- McStudent/MainWindow.xaml.cs
+++ McStudent/MainWindow.xaml.cs
@@ -39,9 +39,6 @@ namespace McStudent
             this.main_frame.Content = new ListePromo();
         }
 
-        private void btn_login_Click(object sender, RoutedEventArgs e)
-        {
-            this.main_frame.Content = new Login();
-        }
+
     }
 }
