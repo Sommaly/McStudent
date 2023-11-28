2ac8839b8b6db788d77ba5136fbe0ca1d8a66607
Sommaly <vanh.sommaly@gmail.com>
2023-11-28 17:03:48 +0100

    taches

--- McStudent/Classe/Tache.cs
+++ McStudent/Classe/Tache.cs
@@ -11,19 +11,15 @@ namespace McStudent.Classe
         public int id { get; set; }
         public string nom {  get; set; }
         public int points { get; set; }
-        public string commentaire {  get; set; }
         public Boolean isObligatoire {  get; set; }
-        public string reponse {  get; set; }
         public TP TP { get; set; }
-        public Tache(int id,string nom, int points, string commentaire, Boolean isObligatoire, string reponse)
+        public Tache(int id,string nom, int points, Boolean isObligatoire, TP TP)
         {
             this.id = id;
             this.nom = nom;
             this.points = points;
-            this.commentaire = commentaire;
             this.isObligatoire = isObligatoire;
-            this.reponse = reponse;
-            this.TP = null;
+            this.TP = TP;
         }
     }
 }
