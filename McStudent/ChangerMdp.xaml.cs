2fdd2601a5e6ddd4805210cf62f959227b6121d2
Ralsywy <bichonkevin4@gmail.com>
2023-11-28 17:04:28 +0100

    aa

--- McStudent/ChangerMdp.xaml.cs
+++ McStudent/ChangerMdp.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,81 @@ namespace McStudent
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isValid())
+            {
+                if (validateUserCredentials(tbx_pseudo.Text, tbx_oldmdp.Password.ToString())) 
+                {
+                    changePassword(tbx_pseudo.Text, tbx_newmdp2.Password.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("Cratère invalide");
+                }
+            }
+        }
+
+        private bool isValid()
+        {
+            if (string.IsNullOrEmpty(tbx_pseudo.Text))
+            {
+                MessageBox.Show("peusdo pastèque non");
+                return false;
+            }
+            else if (string.IsNullOrEmpty(tbx_oldmdp.Password.ToString()))
+            {
+                MessageBox.Show("Mot de passe reiedrqu");
+                return false;
+            } 
+            else if (string.IsNullOrEmpty(tbx_newmdp.Password.ToString()))
+            {
+                MessageBox.Show("Nouveau peau de passe reiqiured");
+                return false;
+            }
+            else if (string.IsNullOrEmpty(tbx_newmdp2.Password.ToString()))
+            {
+                MessageBox.Show("peud o passe reuqired");
+                return false;
+            }
+            else if (tbx_newmdp != tbx_newmdp2)
+            {
+                MessageBox.Show("il faut les même peua de pmase");
+                return false;
+            }
+            return true;
+        }
+        private bool validateUserCredentials(string pseudo, string password)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=mcstudent;Integrated Security=SSPI"))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.eleve WHERE pseudo = '" + pseudo + "' AND password = '" + password + "'", con);
+                con.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    return true;
+                } else { return false; }
+            }
+
+        }
+
+        private void changePassword (string pseudo, string newPassword)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=mcstudent;Integrated Security=SSPI"))
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE dbo.eleve SET PASSWORD = '" + newPassword + "' WHERE pseudo = '" + pseudo + "'", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Mot de jaaj changé avec suces");
+            }
+        }
+
     }
 }
